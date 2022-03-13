8c1dfcd2824f5cdfc0c8ec4a685a4cda64694724
PJ <pje@telecommunity.com>
2022-03-13 16:18:36 -0400

    Display menus above old popovers/under new ones

--- src/Navigator.js
+++ src/Navigator.js
@@ -80,10 +80,7 @@ export class Navigator extends Component {
     openMenu(evt) {
         if (!this.states.length) return;
         const menu = createMenu(this.app);
-        menu.dom.style.setProperty(
-            // Allow popovers (hover preview) to overlay this menu
-            "--layer-menu", getComputedStyle(document.body).getPropertyValue("--layer-popover")-1
-        );
+        menu.dom.addClass("pane-relief-history-menu");
         menu.dom.on("mousedown", ".menu-item", e => {e.stopPropagation();}, true);
         this.states.map(this.formatState.bind(this)).forEach(
             (info, idx) => this.menuItem(info, idx, menu)
