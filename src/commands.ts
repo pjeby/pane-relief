5f88d323941d68533c80213a485afd965846f2a8
PJ <pje@telecommunity.com>
2022-07-10 04:14:58 -0400

    Type-checking for commands

--- src/commands.ts
+++ src/commands.ts
@@ -30,7 +30,7 @@ export function command(id: string, name: string, hotkeys: KeyDef | KeyDef[] = [
 
 export function addCommands<P extends Plugin>(
     plugin: P,
-    cmdset: Record<symbol, (thisArg: P) => boolean | (() => any)>
+    cmdset: Record<symbol, (thisArg: P) => boolean | (() => any)> = plugin.constructor.prototype
 ) {
     // Extract command symbols from cmdset and register them, bound to the plugin for methods
     Object.getOwnPropertySymbols(cmdset).forEach(sym => {
--- src/pane-relief.ts
+++ src/pane-relief.ts
@@ -66,58 +66,57 @@ export default class PaneRelief extends Plugin {
                 app.workspace.on("pane-relief:update-history", (leaf, history) => this.nav.forLeaf(leaf).onUpdateHistory(leaf, history))
             );
         });
+        addCommands(this);
+    }
 
-        addCommands(this, {
-            [command("swap-prev", "Swap pane with previous in split",  "Mod+Shift+PageUp")]   (){ return this.leafPlacer(-1); },
-            [command("swap-next", "Swap pane with next in split",      "Mod+Shift+PageDown")] (){ return this.leafPlacer( 1); },
+    [command("swap-prev", "Swap pane with previous in split",  "Mod+Shift+PageUp")]   (){ return this.leafPlacer(-1); }
+    [command("swap-next", "Swap pane with next in split",      "Mod+Shift+PageDown")] (){ return this.leafPlacer( 1); }
 
-            [command("go-prev",  "Cycle to previous workspace pane",   "Mod+PageUp"  )] () { return () => this.gotoNthLeaf(-1, true); },
-            [command("go-next",  "Cycle to next workspace pane",       "Mod+PageDown")] () { return () => this.gotoNthLeaf( 1, true); },
+    [command("go-prev",  "Cycle to previous workspace pane",   "Mod+PageUp"  )] () { return () => this.gotoNthLeaf(-1, true); }
+    [command("go-next",  "Cycle to next workspace pane",       "Mod+PageDown")] () { return () => this.gotoNthLeaf( 1, true); }
 
-            [command("win-prev", "Cycle to previous window", [] )] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(-1, true); },
-            [command("win-next", "Cycle to next window",     [] )] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow( 1, true); },
+    [command("win-prev", "Cycle to previous window", [] )] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(-1, true); }
+    [command("win-next", "Cycle to next window",     [] )] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow( 1, true); }
 
-            [command("go-1st",   "Jump to 1st pane in the workspace",  "Alt+1")] () { return () => this.gotoNthLeaf(0); },
-            [command("go-2nd",   "Jump to 2nd pane in the workspace",  "Alt+2")] () { return () => this.gotoNthLeaf(1); },
-            [command("go-3rd",   "Jump to 3rd pane in the workspace",  "Alt+3")] () { return () => this.gotoNthLeaf(2); },
-            [command("go-4th",   "Jump to 4th pane in the workspace",  "Alt+4")] () { return () => this.gotoNthLeaf(3); },
-            [command("go-5th",   "Jump to 5th pane in the workspace",  "Alt+5")] () { return () => this.gotoNthLeaf(4); },
-            [command("go-6th",   "Jump to 6th pane in the workspace",  "Alt+6")] () { return () => this.gotoNthLeaf(5); },
-            [command("go-7th",   "Jump to 7th pane in the workspace",  "Alt+7")] () { return () => this.gotoNthLeaf(6); },
-            [command("go-8th",   "Jump to 8th pane in the workspace",  "Alt+8")] () { return () => this.gotoNthLeaf(7); },
-            [command("go-last",  "Jump to last pane in the workspace", "Alt+9")] () { return () => this.gotoNthLeaf(99999999); },
+    [command("go-1st",   "Jump to 1st pane in the workspace",  "Alt+1")] () { return () => this.gotoNthLeaf(0); }
+    [command("go-2nd",   "Jump to 2nd pane in the workspace",  "Alt+2")] () { return () => this.gotoNthLeaf(1); }
+    [command("go-3rd",   "Jump to 3rd pane in the workspace",  "Alt+3")] () { return () => this.gotoNthLeaf(2); }
+    [command("go-4th",   "Jump to 4th pane in the workspace",  "Alt+4")] () { return () => this.gotoNthLeaf(3); }
+    [command("go-5th",   "Jump to 5th pane in the workspace",  "Alt+5")] () { return () => this.gotoNthLeaf(4); }
+    [command("go-6th",   "Jump to 6th pane in the workspace",  "Alt+6")] () { return () => this.gotoNthLeaf(5); }
+    [command("go-7th",   "Jump to 7th pane in the workspace",  "Alt+7")] () { return () => this.gotoNthLeaf(6); }
+    [command("go-8th",   "Jump to 8th pane in the workspace",  "Alt+8")] () { return () => this.gotoNthLeaf(7); }
+    [command("go-last",  "Jump to last pane in the workspace", "Alt+9")] () { return () => this.gotoNthLeaf(99999999); }
 
-            [command("win-1st",   "Switch to 1st window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(0); },
-            [command("win-2nd",   "Switch to 2nd window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(1); },
-            [command("win-3rd",   "Switch to 3rd window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(2); },
-            [command("win-4th",   "Switch to 4th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(3); },
-            [command("win-5th",   "Switch to 5th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(4); },
-            [command("win-6th",   "Switch to 6th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(5); },
-            [command("win-7th",   "Switch to 7th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(6); },
-            [command("win-8th",   "Switch to 8th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(7); },
-            [command("win-last",  "Switch to last window", [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(99999999); },
+    [command("win-1st",   "Switch to 1st window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(0); }
+    [command("win-2nd",   "Switch to 2nd window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(1); }
+    [command("win-3rd",   "Switch to 3rd window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(2); }
+    [command("win-4th",   "Switch to 4th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(3); }
+    [command("win-5th",   "Switch to 5th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(4); }
+    [command("win-6th",   "Switch to 6th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(5); }
+    [command("win-7th",   "Switch to 7th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(6); }
+    [command("win-8th",   "Switch to 8th window",  [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(7); }
+    [command("win-last",  "Switch to last window", [])] () { if (app.workspace.floatingSplit?.children.length) return () => this.gotoNthWindow(99999999); }
 
-            [command("put-1st",  "Place as 1st pane in the split",     "Mod+Alt+1")] () { return () => this.placeLeaf(0, false); },
-            [command("put-2nd",  "Place as 2nd pane in the split",     "Mod+Alt+2")] () { return () => this.placeLeaf(1, false); },
-            [command("put-3rd",  "Place as 3rd pane in the split",     "Mod+Alt+3")] () { return () => this.placeLeaf(2, false); },
-            [command("put-4th",  "Place as 4th pane in the split",     "Mod+Alt+4")] () { return () => this.placeLeaf(3, false); },
-            [command("put-5th",  "Place as 5th pane in the split",     "Mod+Alt+5")] () { return () => this.placeLeaf(4, false); },
-            [command("put-6th",  "Place as 6th pane in the split",     "Mod+Alt+6")] () { return () => this.placeLeaf(5, false); },
-            [command("put-7th",  "Place as 7th pane in the split",     "Mod+Alt+7")] () { return () => this.placeLeaf(6, false); },
-            [command("put-8th",  "Place as 8th pane in the split",     "Mod+Alt+8")] () { return () => this.placeLeaf(7, false); },
-            [command("put-last", "Place as last pane in the split",    "Mod+Alt+9")] () { return () => this.placeLeaf(99999999, false); },
+    [command("put-1st",  "Place as 1st pane in the split",     "Mod+Alt+1")] () { return () => this.placeLeaf(0, false); }
+    [command("put-2nd",  "Place as 2nd pane in the split",     "Mod+Alt+2")] () { return () => this.placeLeaf(1, false); }
+    [command("put-3rd",  "Place as 3rd pane in the split",     "Mod+Alt+3")] () { return () => this.placeLeaf(2, false); }
+    [command("put-4th",  "Place as 4th pane in the split",     "Mod+Alt+4")] () { return () => this.placeLeaf(3, false); }
+    [command("put-5th",  "Place as 5th pane in the split",     "Mod+Alt+5")] () { return () => this.placeLeaf(4, false); }
+    [command("put-6th",  "Place as 6th pane in the split",     "Mod+Alt+6")] () { return () => this.placeLeaf(5, false); }
+    [command("put-7th",  "Place as 7th pane in the split",     "Mod+Alt+7")] () { return () => this.placeLeaf(6, false); }
+    [command("put-8th",  "Place as 8th pane in the split",     "Mod+Alt+8")] () { return () => this.placeLeaf(7, false); }
+    [command("put-last", "Place as last pane in the split",    "Mod+Alt+9")] () { return () => this.placeLeaf(99999999, false); }
 
-            [command("maximize", "Maximize active pane (Toggle)", [])] (this: PaneRelief) {
-                if (this.max.parentForLeaf(app.workspace.activeLeaf)) return () => this.max.toggleMaximize();
-            },
-        });
+    [command("maximize", "Maximize active pane (Toggle)", [])] () {
+        if (this.max.parentForLeaf(app.workspace.activeLeaf)) return () => this.max.toggleMaximize();
     }
 
     onunload() {
         this.app.workspace.unregisterHoverLinkSource(Navigator.hoverSource);
     }
 
-    gotoNthLeaf(n: number, relative: boolean) {
+    gotoNthLeaf(n: number, relative?: boolean) {
         let leaf = app.workspace.activeLeaf;
         const root = leaf.getRoot();
         if (root === app.workspace.leftSplit || root === app.workspace.rightSplit) {
@@ -129,7 +128,7 @@ export default class PaneRelief extends Plugin {
         !leaf || this.app.workspace.setActiveLeaf(leaf, true, true);
     }
 
-    gotoNthWindow(n: number, relative: boolean) {
+    gotoNthWindow(n: number, relative?: boolean) {
         const nav = gotoNth(this.nav.forAll(), this.nav.forLeaf(app.workspace.activeLeaf), n, relative);
         const leaf = nav?.latestLeaf();
         if (leaf) app.workspace.setActiveLeaf(leaf, true, true);
